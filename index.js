18e640f935e90cc094c39f98384e0a4f22f85329
Uche <ucheokeke@gmail.com>
2024-01-13 21:53:28 +0100

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
  var express=require('express');
 var app=express();
+var cors=require('cors');
 
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello, Vercel! This is a simple Node.js server deployed on Vercel.');
 });
@@ -9,3 +11,5 @@ const PORT = process.env.PORT || 3009;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports=app;
