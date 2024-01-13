427bcd8bc70688cd6ba3512a60967a7cbfd40321
uche <119147590+uche9@users.noreply.github.com>
2024-01-13 21:42:30 +0100

    Update index.js
    vbvbv

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
  var express=require('express');
 var app=express();
-const PORT = process.env.PORT || 3009;
 
 app.get('/', (req, res) => {
   res.send('Hello, Vercel! This is a simple Node.js server deployed on Vercel.');
 });
+const PORT = process.env.PORT || 3009;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
